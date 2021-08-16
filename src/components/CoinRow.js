39f9123840fede7310607535706756de058c68e5
unknown <iden.ticlla@gmail.com>
2021-08-16 08:22:32 -0400

    price change red or green

--- src/components/CoinRow.js
+++ src/components/CoinRow.js
@@ -1,20 +1,29 @@
-import React from 'react'
+import React from "react";
 
-const CoinRow = ({coin, index}) => {
-    return (
-      <tr>
-        <td>{index}</td>
-        <td>
-            <img src={coin.image} style={{width: '3%'}} className="img-fluid me-4"></img>
-          <span>{coin.name}</span>
-          <span className="ms-3 text-muted text-uppercase">{coin.symbol}</span>
-        </td>
-        <td>{coin.current_price}</td>
-        <td>{coin.price_change_percentage_24h}</td>
-        <td>{coin.total_volume}</td>
-      </tr>
-    );
-}
+const CoinRow = ({ coin, index }) => {
+  return (
+    <tr>
+      <td>{index}</td>
+      <td>
+        <img
+          src={coin.image}
+          style={{ width: "3%" }}
+          className="img-fluid me-4"
+        ></img>
+        <span>{coin.name}</span>
+        <span className="ms-3 text-muted text-uppercase">{coin.symbol}</span>
+      </td>
+      <td>{coin.current_price}</td>
+      <td
+        className={
+          coin.price_change_percentage_24h > 0 ? "text-success" : "text-danger"
+        }
+      >
+        {coin.price_change_percentage_24h}
+      </td>
+      <td>{coin.total_volume}</td>
+    </tr>
+  );
+};
 
-export default CoinRow
-
+export default CoinRow;
